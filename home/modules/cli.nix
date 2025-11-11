ff096fb75c067b22b4b93d658674679df1d12d78
devluixos <luiizperren@hotmail.com>
2025-11-11 22:16:17 +0100

    nixvim preparation, removed conflict

--- home/modules/cli.nix
+++ home/modules/cli.nix
@@ -2,10 +2,6 @@
 {
   programs.git.enable = true;
   programs.tmux.enable = true;
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
 
   home.packages = with pkgs; [
     yazi
