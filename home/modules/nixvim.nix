5888a88d6d31b06e80205df2ca57087969e6876a
devluixos <luiizperren@hotmail.com>
2025-11-30 19:15:13 +0100

    update: 2025-11-30T19:14:45+01:00

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -179,6 +179,18 @@
         };
       };
 
+      "indent-blankline" = {
+        enable = true;
+        settings = {
+          indent = { char = "▏"; tab_char = "▏"; };
+          scope = {
+            enabled = true;
+            show_start = true;
+            show_end = false;
+          };
+        };
+      };
+
       lualine = {
         enable = true;
         settings = {
