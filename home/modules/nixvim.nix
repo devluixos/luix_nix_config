c362b2f39b6ee7db89a7813052fa1a0d98d01df7
devluixos <luiizperren@hotmail.com>
2025-11-30 17:46:05 +0100

    update: 2025-11-30T17:45:41+01:00

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -82,6 +82,32 @@
         action = ":q<CR>";
         options.silent = false;
       }
+
+      #telescope
+      {
+        mode = "n";
+        key = "<leader>ff"; 
+        action = "<cmd>Telescope find_files<CR>";
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>fg"; 
+        action = "<cmd>Telescope live_grep<CR>";
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>fb"; 
+        action = "<cmd>Telescope buffers<CR>";
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>fh"; 
+        action = "<cmd>Telescope help_tags<CR>";
+        options.silent = true;
+      }
     ];
 
     plugins = {
