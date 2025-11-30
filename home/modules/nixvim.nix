090544c7599840455fe9029960269e5f64193735
devluixos <luiizperren@hotmail.com>
2025-11-30 16:03:09 +0100

    update: 2025-11-30T16:01:13+01:00

--- home/modules/nixvim.nix
+++ home/modules/nixvim.nix
@@ -96,23 +96,47 @@
           nixd.enable    = true;
         };
       };
-    };
-
-    extraPlugins = [ pkgs.vimPlugins.snacks-nvim ];
-
-    extraConfigLua = ''
-      require("lazy").setup({
-        {
-          "folke/snacks.nvim",
-          priority = 1000,
-          lazy = false, --load immediatly
-          opts = {
-            dashboard = { enable = true },
+      alpha = {
+        enable = true;
+        layout = [
+          { type = "padding"; val = 4; }
+          {
+            type = "text";
+            opts = { position = "center"; hl = "Type"; };
+            val = [
+              "  LLLLLLLLLLL                                 iiii                      BBBBBBBBBBBBBBBBB     iiii          tttt                           "
+              "  L:::::::::L                                i::::i                     B::::::::::::::::B   i::::i      ttt:::t                           "
+              "  L:::::::::L                                 iiii                      B::::::BBBBBB:::::B   iiii       t:::::t                           "
+              "  LL:::::::LL                                                           BB:::::B     B:::::B             t:::::t                           "
+              "    L:::::L               uuuuuu    uuuuuu  iiiiiii xxxxxxx      xxxxxxx  B::::B     B:::::Biiiiiiittttttt:::::ttttttt        ssssssssss   "
+              "    L:::::L               u::::u    u::::u  i:::::i  x:::::x    x:::::x   B::::B     B:::::Bi:::::it:::::::::::::::::t      ss::::::::::s  "
+              "    L:::::L               u::::u    u::::u   i::::i   x:::::x  x:::::x    B::::BBBBBB:::::B  i::::it:::::::::::::::::t    ss:::::::::::::s "
+              "    L:::::L               u::::u    u::::u   i::::i    x:::::xx:::::x     B:::::::::::::BB   i::::itttttt:::::::tttttt    s::::::ssss:::::s"
+              "    L:::::L               u::::u    u::::u   i::::i     x::::::::::x      B::::BBBBBB:::::B  i::::i      t:::::t           s:::::s  ssssss "
+              "    L:::::L               u::::u    u::::u   i::::i      x::::::::x       B::::B     B:::::B i::::i      t:::::t             s::::::s      "
+              "    L:::::L               u::::u    u::::u   i::::i      x::::::::x       B::::B     B:::::B i::::i      t:::::t                s::::::s   "
+              "    L:::::L         LLLLLLu:::::uuuu:::::u   i::::i     x::::::::::x      B::::B     B:::::B i::::i      t:::::t    ttttttssssss   s:::::s "
+              "  LL:::::::LLLLLLLLL:::::Lu:::::::::::::::uui::::::i   x:::::xx:::::x   BB:::::BBBBBB::::::Bi::::::i     t::::::tttt:::::ts:::::ssss::::::s"
+              "  L::::::::::::::::::::::L u:::::::::::::::ui::::::i  x:::::x  x:::::x  B:::::::::::::::::B i::::::i     tt::::::::::::::ts::::::::::::::s "
+              "  L::::::::::::::::::::::L  uu::::::::uu:::ui::::::i x:::::x    x:::::x B::::::::::::::::B  i::::::i       tt:::::::::::tt s:::::::::::ss  "
+              "  LLLLLLLLLLLLLLLLLLLLLLLL    uuuuuuuu  uuuuiiiiiiiixxxxxxx      xxxxxxxBBBBBBBBBBBBBBBBB   iiiiiiii         ttttttttttt    sssssssssss    "
+            ];
           }
-        }
-      })
-    '';
+          { type = "padding"; val = 2; }
+          {
+            type = "group";
+            val = [
+              { type = "button"; val = "  Find file"; on_press = "Telescope find_files"; }
+              { type = "button"; val = "  Live grep"; on_press = "Telescope live_grep"; }
+              { type = "button"; val = "  File tree"; on_press = "NvimTreeToggle"; }
+              { type = "button"; val = "  Quit";      on_press = "qa"; }
+            ];
+            opts = { spacing = 1; };
+          }
+        ];
+      };
 
+    };
 
   };
 }
