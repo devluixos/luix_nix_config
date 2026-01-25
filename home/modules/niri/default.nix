1d3e188367ffb1a87a9af958448c7805b891a560
devluixos <luiizperren@hotmail.com>
2026-01-25 18:23:02 +0100

    update: 2026-01-25T18:22:34+01:00

--- home/modules/niri/default.nix
+++ home/modules/niri/default.nix
@@ -1,7 +1,33 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   mainOutputName = "HDMI-A-2";
   verticalOutputName = "HDMI-A-3";
+  baseConfig = builtins.readFile "${pkgs.niri.doc}/share/doc/niri/default-config.kdl";
+  noWaybarConfig = lib.replaceStrings [
+    "spawn-at-startup \"waybar\"\n"
+  ] [
+    ""
+  ] baseConfig;
+  noctaliaLauncherBinds = ''
+    Mod+Space hotkey-overlay-title="Noctalia: Launcher" { spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "toggle"; }
+    Mod+D hotkey-overlay-title="Noctalia: Launcher" { spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "toggle"; }
+    Mod+S hotkey-overlay-title="Noctalia: Control Center" { spawn "qs" "-c" "noctalia-shell" "ipc" "call" "controlCenter" "toggle"; }
+    Mod+Comma hotkey-overlay-title="Noctalia: Settings" { spawn "qs" "-c" "noctalia-shell" "ipc" "call" "settings" "toggle"; }
+    Mod+V hotkey-overlay-title="Noctalia: Clipboard" { spawn "qs" "-c" "noctalia-shell" "ipc" "call" "launcher" "clipboard"; }
+  '';
+  noctaliaLockBind = ''
+    Super+Alt+L hotkey-overlay-title="Noctalia: Lock" { spawn "qs" "-c" "noctalia-shell" "ipc" "call" "lockScreen" "lock"; }
+  '';
+  noFuzzelConfig = lib.replaceStrings [
+    "    Mod+D hotkey-overlay-title=\"Run an Application: fuzzel\" { spawn \"fuzzel\"; }\n"
+  ] [
+    "    ${noctaliaLauncherBinds}\n"
+  ] noWaybarConfig;
+  noctaliaConfig = lib.replaceStrings [
+    "    Super+Alt+L hotkey-overlay-title=\"Lock the Screen: swaylock\" { spawn \"swaylock\"; }\n"
+  ] [
+    "    ${noctaliaLockBind}\n"
+  ] noFuzzelConfig;
 in
 {
   imports = [
@@ -15,27 +41,25 @@ in
     ./noctalia
     ./polkit
     ./screenshot
-    ./waybar
     ./xwayland
     ./xdg
   ];
 
-  programs.fuzzel.enable = true;
+  xdg.configFile."niri/config.kdl".text =
+    noctaliaConfig
+    + ''
 
-  xdg.configFile."niri/config.kdl".text = ''
-    include "${pkgs.niri.doc}/share/doc/niri/default-config.kdl"
+      output "${mainOutputName}" {
+          mode "3440x1440@100.000"
+          position x=0 y=0
+          focus-at-startup
+      }
 
-    output "${mainOutputName}" {
-        mode "3440x1440@100.000"
-        position x=0 y=0
-        focus-at-startup
-    }
-
-    output "${verticalOutputName}" {
-        mode "3840x2160@59.997"
-        scale 1.25
-        transform "270"
-        position x=3440 y=1440
-    }
-  '';
+      output "${verticalOutputName}" {
+          mode "3840x2160@59.997"
+          scale 1.25
+          transform "270"
+          position x=3440 y=1440
+      }
+    '';
 }
