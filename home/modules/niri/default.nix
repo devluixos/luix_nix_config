4fe9c79eaa314a1f56902aa0cf72aa5ef4305269
devluixos <luiizperren@hotmail.com>
2026-01-25 11:08:31 +0100

    niri display setup

--- home/modules/niri/default.nix
+++ home/modules/niri/default.nix
@@ -1,4 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  mainOutputName = "DP-1";
+  verticalOutputName = "HDMI-A-1";
+in
 {
   programs.fuzzel.enable = true;
+
+  xdg.configFile."niri/config.kdl".text = ''
+    include "${pkgs.niri.doc}/share/doc/niri/default-config.kdl"
+
+    output "${mainOutputName}" {
+        mode "3440x1440@100.000"
+        position x=0 y=0
+        focus-at-startup
+    }
+
+    output "${verticalOutputName}" {
+        mode "3440x1440@60.000"
+        transform "90"
+        position x=3440 y=720
+    }
+  '';
 }
