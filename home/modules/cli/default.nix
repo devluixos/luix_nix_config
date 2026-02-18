a478db5a6a0473e4309c4d32f096b43332581da2
Luix <luix@users.noreply.github.com>
2026-02-18 15:44:45 +0100

    git push test

--- home/modules/cli/default.nix
+++ home/modules/cli/default.nix
@@ -1,6 +1,10 @@
 { pkgs, ... }:
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Luix";
+    userEmail = "luix@users.noreply.github.com";
+  };
   programs.tmux.enable = true;
 
   home.packages = with pkgs; [
