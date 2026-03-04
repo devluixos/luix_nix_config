5218ea1988b4e6fb4ea8644813b8c3e3daabff25
Luiz Perren <luiz.perren@siga.swiss>
2026-03-04 16:23:24 +0100

    persisting noctalia setup

--- home/modules/niri/noctalia/default.nix
+++ home/modules/niri/noctalia/default.nix
@@ -7,5 +7,22 @@
   programs.noctalia-shell = {
     enable = true;
     systemd.enable = true;
+    settings = ./settings.json;
+    colors = ./colors.json;
+    plugins = ./plugins.json;
+    pluginSettings = {
+      catwalk = ./plugins/catwalk/settings.json;
+      "fancy-audiovisualizer" = ./plugins/fancy-audiovisualizer/settings.json;
+      "ip-monitor" = ./plugins/ip-monitor/settings.json;
+      "keybind-cheatsheet" = ./plugins/keybind-cheatsheet/settings.json;
+      "model-usage" = ./plugins/model-usage/settings.json;
+      "privacy-indicator" = ./plugins/privacy-indicator/settings.json;
+      todo = ./plugins/todo/settings.json;
+    };
+  };
+
+  xdg.configFile."noctalia/colorschemes" = {
+    source = ./colorschemes;
+    recursive = true;
   };
 }
