06086fa9e20c0d1192ca1b87421d462714413fcb
Luiz Perren <dummy@example.invalid>
2026-05-11 13:19:51 +0200

    update: 2026-05-11T13:19:29+02:00

--- hosts/features/virt-manager.nix
+++ hosts/features/virt-manager.nix
@@ -19,4 +19,30 @@
   ];
 
   networking.firewall.trustedInterfaces = [ "virbr0" ];
+
+  systemd.services.libvirt-default-network = {
+    description = "Start the libvirt default NAT network";
+    wantedBy = [ "multi-user.target" ];
+    requires = [ "libvirtd.service" ];
+    after = [
+      "libvirtd-config.service"
+      "libvirtd.service"
+    ];
+    path = with pkgs; [
+      gnugrep
+      libvirt
+    ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      virsh -c qemu:///system net-info default >/dev/null
+      virsh -c qemu:///system net-autostart default
+
+      if ! virsh -c qemu:///system net-info default | grep -q '^Active:[[:space:]]*yes'; then
+        virsh -c qemu:///system net-start default
+      fi
+    '';
+  };
 }
