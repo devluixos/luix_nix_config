e5210dc6394c7ec1f1aedd9c66a16b3ccecc5944
Luix <luix@users.noreply.github.com>
2026-06-02 10:49:19 +0200

    update: 2026-06-02T10:48:22+02:00

--- home/modules/kitty/default.nix
+++ home/modules/kitty/default.nix
@@ -19,7 +19,7 @@
       cursor_trail_start_threshold = "2";
       mouse_hide_wait = "2.0";
 
-      url_color = "#8ea4a2";
+      url_color = "#0087bd";
       url_style = "curly";
       detect_urls = "yes";
       show_hyperlink_targets = "yes";
@@ -32,7 +32,7 @@
       enable_audio_bell = "yes";
       visual_bell_duration = "0.0";
       window_alert_on_bell = "yes";
-      bell_on_tab = "kanagawa ";
+      bell_on_tab = "🔔 ";
 
       remember_window_size = "yes";
       remember_window_position = "no";
@@ -58,17 +58,17 @@
       notify_on_cmd_finish = "unfocused 10.0 bell";
     };
     extraConfig = ''
-      ## name: Kanagawa Dragon
-      ## author: Noctalia community palette, adapted by Luix
+      ## name: VividPunk
+      ## author: CarleScript
       ## license: MIT
-      ## blurb: Dark, mossy Kanagawa palette matched to Noctalia.
+      ## blurb: Vivid theme based on the colors of my wallpaper created by 00xBAD (https://wallhaven.cc/w/gpy1k3)
 
       #: The basic colors
 
-      foreground                      #c5c9c5
-      background                      #181616
-      selection_foreground            #181616
-      selection_background            #c5c9c5
+      foreground                      #F88132
+      background                      #171A26
+      selection_foreground            #171A26
+      selection_background            #F88132
 
 
       #: Text opacity on inactive kitty windows (0.0 to 1.0)
@@ -78,20 +78,20 @@
 
       #: Cursor colors
 
-      cursor                          #c5c9c5
+      cursor                          #F88132
       cursor_text_color               background
 
 
       #: URL underline color when hovering with mouse
 
-      url_color                       #8ea4a2
+      url_color                       #F88132
 
 
       #: kitty window border colors and terminal bell colors
 
-      active_border_color             #8a9a7b
-      inactive_border_color           #282727
-      bell_border_color               #c4b28a
+      active_border_color             #F88132
+      inactive_border_color           #171A26
+      bell_border_color               #F88132
       visual_bell_color               none
 
 
@@ -103,58 +103,58 @@
 
       #: Tab bar colors
 
-      active_tab_foreground           #181616
-      active_tab_background           #8a9a7b
-      inactive_tab_foreground         #c8c093
-      inactive_tab_background         #282727
-      tab_bar_background              #181616
+      active_tab_foreground           #171A26
+      active_tab_background           #F88132
+      inactive_tab_foreground         #F88132
+      inactive_tab_background         #171A26
+      tab_bar_background              #171A26
       tab_bar_margin_color            none
-      tab_separator                   " ┆ "
+      tab_separator                   " | "
 
 
       #: Colors for marks (marked text in the terminal)
 
-      mark1_foreground                #181616
-      mark1_background                #8a9a7b
-      mark2_foreground                #181616
-      mark2_background                #c4b28a
-      mark3_foreground                #181616
-      mark3_background                #8ea4a2
+      mark1_foreground                #171A26
+      mark1_background                #F88132
+      mark2_foreground                #171A26
+      mark2_background                #F88132
+      mark3_foreground                #171A26
+      mark3_background                #F88132
 
 
       #: The basic 16 colors
 
       #: black
-      color0                          #282727
-      color8                          #625e5a
+      color0                          #010101
+      color8                          #757575
 
       #: red
-      color1                          #c4746e
-      color9                          #c4746e
+      color1                          #A32522
+      color9                          #D53B3E
 
       #: green
-      color2                          #8a9a7b
-      color10                         #8a9a7b
+      color2                          #115A24
+      color10                         #0A8439
 
       #: yellow
-      color3                          #c4b28a
-      color11                         #b6927b
+      color3                          #D5A243
+      color11                         #F5BF59
 
       #: blue
-      color4                          #8ba4b0
-      color12                         #8ba4b0
+      color4                          #263788
+      color12                         #6C85E8
 
       #: magenta
-      color5                          #a292a3
-      color13                         #a292a3
+      color5                          #793664
+      color13                         #B75492
 
       #: cyan
-      color6                          #8ea4a2
-      color14                         #8ea4a2
+      color6                          #128CA1
+      color14                         #7ACCD4
 
       #: white
-      color7                          #c5c9c5
-      color15                         #c8c093
+      color7                          #DADAD7
+      color15                         #F3F3F2
     '';
   };
 }
