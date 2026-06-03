3a8658216b8eaa053526744fa5c22680efd2e2bf
Luix <luix@users.noreply.github.com>
2026-06-03 11:32:25 +0200

    update: 2026-06-03T11:31:56+02:00

--- home/modules/fish/default.nix
+++ home/modules/fish/default.nix
@@ -4,37 +4,22 @@
     enable = true;
 
     functions.fish_greeting = ''
-      set -l green (set_color 8ccf7e)
+      set -l pine (set_color 5f8d62)
+      set -l leaf (set_color 8ccf7e)
       set -l moss (set_color 6f8f5f)
       set -l bark (set_color a58f6f)
-      set -l dim (set_color 6b7f6a)
+      set -l mist (set_color 6b7f6a)
+      set -l hello (set_color d6e7b5)
       set -l normal (set_color normal)
 
-      set -l where (prompt_pwd)
-      set -l branch
-
-      if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-        set branch (command git branch --show-current 2>/dev/null)
-
-        if test -z "$branch"
-          set branch (command git rev-parse --short HEAD 2>/dev/null)
-        end
-      end
-
       echo
-      printf '%s%s%s%s\n' "$green" '     /\      ' "$moss" 'forest shell'
-      printf '%s%s%s%s\n' "$green" '  /\ /  \    ' "$dim" 'quiet terminal, useful shell'
-      printf '%s  path  %s%s%s\n' "$bark" "$normal" "$where" "$normal"
-
-      if test -n "$branch"
-        printf '%s  git   %s%s%s\n' "$green" "$normal" "$branch" "$normal"
-      end
-
-      if test -n "$IN_NIX_SHELL"
-        printf '%s  nix   %s%s%s\n' "$moss" "$normal" "$IN_NIX_SHELL" "$normal"
-      end
-
-      printf '%s  hint  %s%s%s\n' "$dim" "$normal" 'configure by friction, keep what helps' "$normal"
+      printf '%s%s%s\n' "$pine" '       /\        /\        /\       ' "$normal"
+      printf '%s%s%s\n' "$leaf" '      /  \  /\  /  \  /\  /  \      ' "$normal"
+      printf '%s%s%s\n' "$pine" '     /____\/__\/____\/__\/____\     ' "$normal"
+      printf '%s      ||    ||   %shello%s   ||    ||%s\n' "$bark" "$hello" "$bark" "$normal"
+      printf '%s%s%s\n' "$moss" '   /\ ||        /\        || /\    ' "$normal"
+      printf '%s%s%s\n' "$pine" '  /__\||       /__\       ||/__\   ' "$normal"
+      printf '%s%s%s\n' "$mist" '       ~~~  ~~~  ~~  ~~~  ~~~       ' "$normal"
       echo
     '';
   };
